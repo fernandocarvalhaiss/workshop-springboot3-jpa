4839c415db424e9fec7ce7f46e8d8e0fecde6653
Fernando Carvalhais <fernando.carvalhaiss@gmail.com>
2025-12-14 19:04:23 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.educandoweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.educandoweb.course.entities.User;
 
+@Repository //nesse caso é opcional registrar o componente porque já tem o extends abaixo que indica que herda do JPA
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,21 +1,37 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/users") //endpoint da url
 public class UserResource {
 	
+	@Autowired //injecao automatica de dependencia do servico UserService
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria",  "maria@gmail.com", "1199999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		//User u = new User(1L, "Maria",  "maria@gmail.com", "1199999999", "12345");
+		// endpoint para buscar todos
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);	
 		
+	}
+	
+	@GetMapping(value = "/{id}")  //endpoint da url, passando id para busca por id
+	public ResponseEntity<User> findById(@PathVariable Long id){		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
